5468cbe2faac614c3550cc05aca5cb0107ca019d
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-05-18 22:09:52 +0200

    Remap right option/command

---
+++ configs/remap-right-option-command.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+hidutil property --set '{"UserKeyMapping":
+    [{"HIDKeyboardModifierMappingSrc":0x7000000e7,
+      "HIDKeyboardModifierMappingDst":0x7000000e6},
+     {"HIDKeyboardModifierMappingSrc":0x7000000e6,
+      "HIDKeyboardModifierMappingDst":0x7000000e7}]
+}'
+
